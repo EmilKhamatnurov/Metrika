a1dd128ec251ee806e686d2b2cc9c4d441fc13e7
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-04-16 11:27:09 +0500

    Добавлено меню бургер

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,3 +1,4 @@
+// Данные для открытия меню
 const BURGER_OPENED_CLASSNAME = 'burger_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
 const BODY_FIXED_CLASSNAME = 'body_fixed';
@@ -6,6 +7,12 @@ const bodyNode = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
 const burgerContentNode = document.querySelector('.js-burger__content')
+
+// Данные для анимации бургера
+const BURGER_BTN_FIRST_LINE_ANIMATE_CLASS = 'burger-btn__first-line_animate';
+const BURGER_BTN_SECOND_LINE_ANIMATE_CLASS = 'burger-btn__second-line_animate';	
+const burgerBtnFirstLine = document.querySelector('.burger-btn__first-line');
+const burgerBtnSecondLine = document.querySelector('.burger-btn__second-line');
 
 burgerBtnNode.addEventListener('click', toggleBurger);
 
@@ -21,4 +28,7 @@ function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
     bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+
+	burgerBtnFirstLine.classList.toggle(BURGER_BTN_FIRST_LINE_ANIMATE_CLASS);
+			burgerBtnSecondLine.classList.toggle(BURGER_BTN_SECOND_LINE_ANIMATE_CLASS);
 }
