6796ee1b0ea2515a7a6c869e4202265645f6b200
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-04-16 18:04:58 +0500

    Справлены элементы меню и попапа
    1) Исправлена ссылка на раздел комментариев из меню
    2) Исправлена анимация

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,9 +1,9 @@
 // Данные для открытия меню
 const BURGER_OPENED_CLASSNAME = 'burger_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
-const BODY_FIXED_CLASSNAME = 'body_fixed';
+const BODY_FIXED_CLASSNAME_2 = 'body_fixed';
 
-const bodyNode = document.querySelector('body');
+const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
 const burgerContentNode = document.querySelector('.js-burger__content')
@@ -14,11 +14,31 @@ const BURGER_BTN_SECOND_LINE_ANIMATE_CLASS = 'burger-btn__second-line_animate';
 const burgerBtnFirstLine = document.querySelector('.burger-btn__first-line');
 const burgerBtnSecondLine = document.querySelector('.burger-btn__second-line');
 
+//ДООЛНЕНИЯ. Закрытие меню при нажатии на ссылку
+// var burgerLinks = document.getElementsByClassName(".burger__link");
+// console.log(burgerLinks.item(0));
+
+// for (let i = 0; i < burgerLinks.length; i++) {
+// 	burgerLinks[i].addEventListener('click', toggleBurger);
+//   }
+// let firstLink = burgerLinks[0];
+// let secondLink = burgerLinks[0];
+// let thirdink = burgerLinks[0];
+// let fourthLink = burgerLinks[0];
+
+// Array.prototype.forEach.call(burgerLinks, function(burgerLinks) {
+//   element.addEventListener('click', function() {
+//     burgerNode.classList.remove(BURGER_OPENED_CLASSNAME);
+//     bodyNode.classList.remove(BODY_FIXED_CLASSNAME);
+//     burgerBtnNode.classList.remove(BURGER_BTN_OPENED_CLASSNAME);
+//   });
+// });
+
+
 burgerBtnNode.addEventListener('click', toggleBurger);
-
 burgerNode.addEventListener('click', (event) => {
+	// Закрытие меню при нажатии на область вне меню
     const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
-
     if (isClickOutsideContent) {
         toggleBurger();
     }
@@ -26,9 +46,10 @@ burgerNode.addEventListener('click', (event) => {
 
 function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
-    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
-
+	
+	// Анимация бургера
 	burgerBtnFirstLine.classList.toggle(BURGER_BTN_FIRST_LINE_ANIMATE_CLASS);
-			burgerBtnSecondLine.classList.toggle(BURGER_BTN_SECOND_LINE_ANIMATE_CLASS);
+	burgerBtnSecondLine.classList.toggle(BURGER_BTN_SECOND_LINE_ANIMATE_CLASS);
 }
